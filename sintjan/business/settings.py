9eed6c5d69bba97eeffc2795771ce7478427412c
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2015-10-05 21:24:57 +0200

    end of life

--- sintjan/business/settings.py
+++ sintjan/business/settings.py
@@ -3,62 +3,56 @@ import util
 from google.appengine.api import users
 from datetime import date
 
-
-
 #########################################################
 # A random Header for CSS #
 #########################################################
 headers = ["hoofding.jpg", "hoofding1.jpg","hoofding2.jpg","hoofding3.jpg","hoofding4.jpg","hoofding5.jpg","hoofding6.jpg"]
 
-    
-   
+
 # object definition of Settings
 class RequestContext():
-    #analytics = False
+    # analytics = False
     analytics = True
     url = 'http://www.sintjan-lommel.be'
-    #url = 'http://sintjanlommel.appspot.com'
+    # url = 'http://sintjanlommel.appspot.com'
     mail_website = 'maarten@sintjan-lommel.be'
     mail_contact = 'contact@sintjan-lommel.be'
     
     def header(self):
-        return "/s/img/headers/" + headers[random.randint(0,6)];
+        return "/s/img/headers/" + headers[random.randint(0, 6)]
         
     def now(self):
-        today = date.today();
-        return util.strdate(today.year,today.month,today.day); 
+        today = date.today()
+        return util.strdate(today.year, today.month, today.day)
     
     def axenroos(self):
-        return axenroosHtml();
+        return axenroosHtml()
     
     def logout(self):
-        return users.create_logout_url("/"); 
+        return users.create_logout_url("/")
     
     def login(self):
-        return users.create_login_url("/admin"); 
+        return users.create_login_url("/admin")
     
     def authOk(self):
         return util.isAdmin()
     
     def user(self):
-        return users.get_current_user();
+        return users.get_current_user()
     
     def contact(self):
         return "contact@sintjan-lommel.be"
-
-    
-            
-    
-
 
 #########################################################
 # Axenroos
 #########################################################
 
 # A list of months, base on their month it an appropriate value will be in the array
-#axenroosConfig = []
-#axenroosConfig = ["","Leeuw", "Bever","Kameel","Uil","Schildpad","Steenbok","","","Wasbeer","Pauw","Poes","Havik"];
-axenroosConfig = ["","Havik", "Poes","Wasbeer","Steenbok","Kameel","Pauw","","","Bever","Uil","Leeuw","Schildpad"];
+# axenroosConfig = []
+# axenroosConfig = ["","Leeuw", "Bever","Kameel","Uil","Schildpad","Steenbok","","","Wasbeer","Pauw","Poes","Havik"];
+# axenroosConfig = ["","Havik", "Poes","Wasbeer","Steenbok","Kameel","Pauw","","","Bever","Uil","Leeuw","Schildpad"];
+axenroosConfig = ["", "Poes", "Kameel", "Uil", "Pauw", "Schildpad", "Wasbeer", "", "",
+                  "Leeuw", "Bever", "Steenbok", "Havik"]
 
 # Pointers to the images
 axenroosArr = {
@@ -76,20 +70,18 @@ axenroosArr = {
 
 
 def axenroosHtml():
-    month = date.today().month;
-    obj = axenroosConfig[month];
+    month = date.today().month
+    obj = axenroosConfig[month]
     
     if obj != "":
-        photo = "/s/img/axenroos/" + axenroosArr[obj];
-        link = "/overons/axenroos/" + obj.lower();
-        naam = obj;
-        return "<a class=\"axenroos\" href=\""+link+"\"><img src=\"" +photo + "\" alt=\"" + naam + "\" width=\"50\" /></a><p class=\"ax\"><a href=\"" + link + "\">" + naam + "</a>";
-    
-    
+        photo = "/s/img/axenroos/" + axenroosArr[obj]
+        link = "/overons/axenroos/" + obj.lower()
+        naam = obj
+        return "<a class=\"axenroos\" href=\"" + link + "\"><img src=\"" + photo + "\" alt=\"" + naam + "\" width=\"50\" /></a><p class=\"ax\"><a href=\"" + link + "\">" + naam + "</a>"
+
     else:
-        return "<p class=\"ax\">Het is vakantie</p>";        
-    
-    return "<!--nada axenroos-->";
+        return "<p class=\"ax\">Het is vakantie</p>"
+
     
 
 
